60664d607fb74e6924714f76304158af82282e53
kikisan <kiketdule@gmail.com>
2024-09-08 11:44:13 +0200

    MEP style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./node_modules/flowbite/**/*.js" // set up the path to the flowbite package
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        col1 : '#f4fefd',
+        col2 : '#0ef6cc',
+        col3 : '#3a4f50',
+        col4 : '#1b2223',
+
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin') // add the flowbite plugin
