49deac7e7807fcb435cab1ad09703e67418e2fd4
kikisan <kiketdule@gmail.com>
2024-09-16 21:52:58 +0200

    db article ok, on va faire crud

--- migrations/Version20240916195224.php
+++ migrations/Version20240916195224.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240916175700 extends AbstractMigration
+final class Version20240916195224 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240916175700 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE types (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE articles ADD visible TINYINT(1) NOT NULL, ADD thumbnail VARCHAR(255) NOT NULL, ADD pitch LONGTEXT NOT NULL, ADD text LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE types');
+        $this->addSql('ALTER TABLE articles DROP visible, DROP thumbnail, DROP pitch, DROP text');
     }
 }
