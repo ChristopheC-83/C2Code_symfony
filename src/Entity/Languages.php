c65b5c606d0872455e27cc370d9ca85d3f88e04c
kikisan <kiketdule@gmail.com>
2024-09-16 18:12:21 +0200

    read language ok

---
+++ src/Entity/Languages.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LanguagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LanguagesRepository::class)]
+class Languages
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $language = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLanguage(): ?string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(string $language): static
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+}
--- src/Repository/LanguagesRepository.php
+++ src/Repository/LanguagesRepository.php
@@ -2,39 +2,39 @@
 
 namespace App\Repository;
 
-use App\Entity\ArticleType;
+use App\Entity\Languages;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<ArticleType>
+ * @extends ServiceEntityRepository<Languages>
  */
-class ArticleTypeRepository extends ServiceEntityRepository
+class LanguagesRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, ArticleType::class);
+        parent::__construct($registry, Languages::class);
     }
 
     //    /**
-    //     * @return ArticleType[] Returns an array of ArticleType objects
+    //     * @return Languages[] Returns an array of Languages objects
     //     */
     //    public function findByExampleField($value): array
     //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
     //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
+    //            ->orderBy('l.id', 'ASC')
     //            ->setMaxResults(10)
     //            ->getQuery()
     //            ->getResult()
     //        ;
     //    }
 
-    //    public function findOneBySomeField($value): ?ArticleType
+    //    public function findOneBySomeField($value): ?Languages
     //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
     //            ->setParameter('val', $value)
     //            ->getQuery()
     //            ->getOneOrNullResult()
