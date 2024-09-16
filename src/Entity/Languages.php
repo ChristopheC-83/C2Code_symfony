8d7c9e5087abe7029c5abcf3ff73864050563eda
kikisan <kiketdule@gmail.com>
2024-09-16 21:57:35 +0200

    crud article créé... au boulot !

--- src/Entity/Languages.php
+++ src/Entity/Languages.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Entity\Creator\Articles;
+use App\Entity\Articles;
 use App\Repository\LanguagesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
--- src/Entity/Types.php
+++ src/Entity/Types.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Entity\Creator\Articles;
+use App\Entity\Articles;
 use App\Repository\TypesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
